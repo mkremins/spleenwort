d15f5809548516b2c80dc656717c216a7a0b033d
lostjs <wjingsen@gmail.com>
2024-05-14 23:27:12 -0700

    added constraint to no more than 2 obstacles

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ def read_random_outline(file_path):
 
 # Generate a unique file name using the story name and timestamp
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
-user_input_text = "cat pirates" #theme of the story
+user_input_text = "magical poems" #theme of the story
 file_name = f"{user_input_text}_{GPT_MODEL}_{timestamp}.txt"
 
 # saving the prints .txt file
@@ -272,7 +272,7 @@ with open(file_name, 'w') as file:
     print("### OUTLINE")
     #outline = random.choice(outlines) # TODO pick at random?>> is it not random? 
     # read randomly from the pregenerated outline file
-    outline = read_random_outline('outlines05142240.txt')
+    outline = read_random_outline('outlines05142315.txt')
     print(outline)
     outline_prompts = promptify_outline(outline, user_input_text)
     print(storify_prompts(outline_prompts))
