d6e05033d48853e89a392caf3ddf2ce5fb2d300c
lostjs <wjingsen@gmail.com>
2024-05-29 16:05:11 -0700

    test: " no more than five sentences"

--- main.py
+++ main.py
@@ -98,11 +98,11 @@ You're writing a story about:
 
 {{user_input_text}}
 
-Write the first paragraph of the story. In this paragraph, {{follow_instruction}}. Use no more than four sentences in the paragraph.
+Write the first paragraph of the story. In this paragraph, {{follow_instruction}}. Use no more than five sentences in the paragraph.
 """.strip()
 
 followup_prompt = """
-Write the next paragraph of the story. Remember the story is about: {{user_input_text}}. In this paragraph, {{follow_instruction}}. Use no more than four sentences in the paragraph.
+Write the next paragraph of the story. Remember the story is about: {{user_input_text}}. In this paragraph, {{follow_instruction}}. Use no more than five sentences in the paragraph.
 """.strip()
 
 # add an extra prompts for obstacles
@@ -199,11 +199,11 @@ You're writing a story about:
 
 {{user_input_text}}
 
-Write the first paragraph of the story. Use no more than four sentences in the paragraph.
+Write the first paragraph of the story. Use no more than five sentences in the paragraph.
 """.strip()
 
 naive_followup_prompt = """
-Write the next paragraph of the story. Remember the story is about: {{user_input_text}}. Use no more than four sentences in the paragraph.
+Write the next paragraph of the story. Remember the story is about: {{user_input_text}}. Use no more than five sentences in the paragraph.
 """.strip()
 
 def promptify_naively(num_paras, user_input_text):
